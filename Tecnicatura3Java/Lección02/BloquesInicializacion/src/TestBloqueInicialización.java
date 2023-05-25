3c9b7fb8ad69b6db66cac6c7606a0a7243853d91
Valentin Albornoz <valeg486@gmail.com>
2023-05-25 15:05:17 -0300

    Clase 7

--- Tecnicatura3Java/Lección02/BloquesInicializacion/src/TestBloqueInicialización.java
+++
@@ -1,6 +0,0 @@
-public class TestBloqueInicialización {
-    public static void main(String[] args) {
-        Persona persona1 = new Persona();
-        System.out.println("persona1= " + persona1);
-    }
-}
