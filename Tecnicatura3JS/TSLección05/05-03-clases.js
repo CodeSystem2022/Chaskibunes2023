bae0a71833780461532a0acd73700ce3ca4d6ec0
MANASES200 <111753045+MANASES200@users.noreply.github.com>
2023-05-29 21:48:05 -0300

    29 MAYO

--- Tecnicatura3JS/TSLección05/05-03-clases.js
+++ Tecnicatura3JS/TSLección05/05-03-clases.js
@@ -18,6 +18,14 @@ class Persona { //Clase Padre
   set apellido(apellido) {
     this._apellido = apellido;
   }
+  nombreCompleto(){
+    return this._nombre+' '+this._apellido;
+  }
+  // Sobreescribiendo el método de la clase padre (Object)
+  toString(){ // Regresa un String
+    // Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
+    return this.nombreCompleto();
+  }
 }
 
 class Empleado extends Persona { // Clase hija
@@ -32,20 +40,34 @@ class Empleado extends Persona { // Clase hija
     set departamento(departamento){
         this._departamento = departamento;
     }
+    // Sobreescritura
+    nombreCompleto(){
+      return super.nombreCompleto()+', '+this._departamento;
+    }
 }
 let persona1 = new Persona("Martín", "Perez");
 console.log(persona1.nombre);
-console.log(persona1.apellido);
-persona1.nombre = "Juan Carlos";
+persona1.nombre = 'Juan Carlos';
 console.log(persona1.nombre);
+// Ahora lo hacemos con los apellidos
+console.log(persona1.apellido);
+persona1.apellido = 'Sanchez';
+console.log(persona1.apellido);
 //console.log(persona1);
 let persona2 = new Persona("Carlos", "Lara");
 console.log(persona2.nombre);
-console.log(persona2.apellido);
-persona2.nombre = "María Laura";
+persona2.nombre = 'María Laura';
 console.log(persona2.nombre);
+// Ahora lo hacemos con los apellidos
+console.log(persona2.apellido);
+persona2.apellido = 'Jonson';
+console.log(persona2.apellido);
 //console.log(persona2);
 
 let empleado1 = new Empleado('María', 'Gimenez', 'Sistemas');
 console.log(empleado1);
-console.log(empleado1.nombre);
+console.log(empleado1.nombreCompleto());
+
+// Object.prototype.toString Esta es la manera de acceder a atributos y métodos de manera dinamica
+console.log(empleado1.toString());
+console.log(persona1.toString());
