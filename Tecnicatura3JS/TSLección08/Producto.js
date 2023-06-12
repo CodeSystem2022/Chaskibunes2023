48552f54adcf9c95cfd181f67b4b21b4278c2659
Gabriel Muñoz Rodriguez <gabriel_13l@hotmail.com>
2023-06-12 20:51:25 -0300

    Create Producto.js
    Se crea el archivo Producto

---
+++ Tecnicatura3JS/TSLección08/Producto.js
@@ -0,0 +1,35 @@
+class Prodcuto{
+
+    static contadorProcutos = 0;
+
+    constructor(nombre,precio){
+        this._idProducto = ++Prodcuto.contadorProcutos;
+        this._nombre = nombre;
+        this._precio = precio;
+    }
+
+    getIdProducto(){
+        return this._idProducto;
+    }
+
+    getNombre(){
+        return this._nombre;
+    }
+
+    getPrecio(){
+        return this._precio;
+    }
+
+    setNombre(){
+        this._nombre = nombre;
+    }
+
+    setPrecio(){
+        this._precio = precio;
+    }
+
+    toString(){
+        return `${this._idProducto}: ${this._nombre} $${this._precio}`
+    }
+
+}
